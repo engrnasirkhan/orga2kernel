3bbb5b9a8f054bc849d1ca79661ae082cf160b60
santiago.pivetta <santiago.pivetta@fde0c828-8c4b-11de-b54f-137c5e29c83a>
2009-11-07 02:44:39 +0000

    En el commit anterior me olvide de agregar types.h que define algunos tipo de datos que usa el kernel. En realidad no use mucho esto, pero podria ser un lugar para ir definiendo los tipos de datos que use el kernel.

---
+++ types.h
@@ -0,0 +1,17 @@
+/*
+ * Aca se definen algunos tipos de datos comunes al kernel
+ */
+ 
+#ifndef __KERNEL_TYPES_H__
+#define __KERNEL_TYPES_H__
+
+typedef unsigned int    uint;
+typedef unsigned char   ubyte;
+typedef char            byte;
+
+//Virtual Address
+typedef unsigned int    va;
+//Physical Address
+typedef unsigned int    pa;
+
+#endif
