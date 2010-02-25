ec64fda90ff2dc033c90a99e3f566d8dadd754a8
santiago.pivetta <santiago.pivetta@fde0c828-8c4b-11de-b54f-137c5e29c83a>
2010-02-25 14:29:40 +0000

    Comentario sobre la dummy gdt corregido xD

--- kernel/src/boot/boot.asm
+++ kernel/src/boot/boot.asm
@@ -22,8 +22,8 @@ section .data
 
     dummy_gdt:
 	    dd 0, 0
-	    db 0xFF, 0xFF, 0, 0, 0, 10011010b, 11001111b, 0x80	; code selector 0x08: base 0x40000000, limit 0xFFFFFFFF, type 0x9A, granularity 0xCF
-	    db 0xFF, 0xFF, 0, 0, 0, 10010010b, 11001111b, 0x80	; data selector 0x10: base 0x40000000, limit 0xFFFFFFFF, type 0x92, granularity 0xCF
+	    db 0xFF, 0xFF, 0, 0, 0, 10011010b, 11001111b, 0x80	; code selector 0x08: base 0x80000000, limit 0xFFFFFFFF, type 0x9A, granularity 0xCF
+	    db 0xFF, 0xFF, 0, 0, 0, 10010010b, 11001111b, 0x80	; data selector 0x10: base 0x80000000, limit 0xFFFFFFFF, type 0x92, granularity 0xCF
 
     dummy_gdt_desc:
 	    dw $ - dummy_gdt 
