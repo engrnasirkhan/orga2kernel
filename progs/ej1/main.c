9e28166350d34c49d7580c2d660e46e8080590fb
ezeaguerre <ezeaguerre@fde0c828-8c4b-11de-b54f-137c5e29c83a>
2010-04-22 18:10:00 +0000

    Tareas con getpid.

--- progs/ej1/main.c
+++ progs/ej1/main.c
@@ -2,19 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-//#include <unistd.h>
+#include <unistd.h>
 
 void main() {
-	int i=0;
-	
-		while(1){
-			i++;
-			i++;
-			for(int a=0; a<1000; a++){
-			 a=a+1;
-			 a--;				
-			}
-			printf("%d \n",i);
-			
-		}
+	pid_t pid = getpid();
+	int i;
+	for ( i = 0; ; i++ )
+		printf( "Proceso %d: %d.               \r", pid, i );
 }
+
+/*
+int main( const char *argv ) {
+	printf ( "Argumentos: %s\n", argv );
+	puts ( "Numes aleatorios:" );
+	for ( int i = 0; i < 10; i++ )
+		printf ( "%d ", rand() );
+
+	if ( !strcmp( argv, "/boot/ej1 Hola Mundo :-)" ) )
+		return 123456789;
+	return 987654321;
+}
+*/
+/*
+int main(int argc, char **argv) {
+	printf ( "Argumentos: %d\n", argc );
+	for ( int i = 0; i < argc; i++ )
+		printf ( "Argumento %d: %s\n", i+1, argv[i] );
+
+	puts ( "Numeros aleatorios:" );
+	srand( time( NULL ) );
+	for ( int i = 0; i < 10; i++ ) {
+		printf ( "%d ", rand() );
+	}
+
+	return 0;
+}*/
--- progs/ej2/main.c
+++ progs/ej2/main.c
@@ -2,25 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-//#include <unistd.h>
+#include <unistd.h>
 
 static int contador;
 
 void main() {
-	
-	int i=1;
-	
-		while(1){
-			i++;
-			i++;
-			for(int a=0; a<1000; a++){
-			 a=a+1;
-			 a--;				
-			}
-			printf("%d \n",i);
-			
-		}
-		
-		
+	pid_t pid = getpid();
+	for ( ; contador < 20000; contador++ ) {
+		printf ( "Proceso %d: %d\n", pid, contador );
+	}
 	exit( 0 );
 }
